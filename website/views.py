8bc5f3780e475d0defe8de7fd3c14c508a8bd97b
janiejestemja <janiejestemja@gmail.com>
2025-10-20 09:23:50 +0200

    refactor templates

--- website/views.py
+++ website/views.py
@@ -8,7 +8,6 @@ from .models import Note, CipherNote
 views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET"])
-@login_required
 def home():
     return render_template("home.html", user=current_user)
 
